c58ce72513f3e1ae1fa6183a6c7d5d7bd912b1fc
Artur <gloory.gom@gmail.com>
2023-03-29 01:21:20 +0600

    sam vozmi

--- keu/app/urls.py
+++ keu/app/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('news/', DetailNews.as_view(), name='news'),
+    path('news/', NewsListView.as_view(), name='news'),
+    path('news/<int:pk>/', DetailNews.as_view(), name='news'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- keu/app/views.py
+++ keu/app/views.py
@@ -21,11 +21,24 @@ class Home(ListView):
         context['news'] = News.objects.order_by('-date')[:3]
         print(context['news'])
         return context
-class DetailNews(ListView):
+class NewsListView(ListView):
     model = News
     queryset = News.objects.all()
     context_object_name = 'news'
     template_name = 'news.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.all()
+        context['programs'] = Program.objects.all()
+        context['aboutkeus'] = AboutKEU.objects.all()
+        context['abouts'] = About.objects.all()
+        return context
+class DetailNews(DetailView):
+    model = News
+    context_object_name = 'news'
+    template_name = 'detail_news.html'
 
     def get_context_data(self, **kwargs):
 
